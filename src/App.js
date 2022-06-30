a6d3061aea8a458da82d6eb2efe93c546cbba83e
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-06-30 13:31:11 +0200

    created first structure for app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import logoVictor from './images/logo-victor.png'
 
 function App() {
   return (
     <div className="App">
+     <div className="logo-container">
+      <img 
+        src={logoVictor} 
+        className = "logo"
+        alt="Personal logo of Victor" />
+     </div>
+
+     <div className="calculator-container">
+      <div className="row"></div>
+      <div className="row"></div>
+      <div className="row"></div>
+      <div className="row"></div>
+      <div className="row"></div>
+     </div>
      
     </div>
   );
